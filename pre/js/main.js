a66f4821e490601c3800b3012c63d6cecf7fba5f
CarlosMunozDiazCSIC <carlos.munoz@cchs.csic.es>
2021-09-09 16:26:24 +0200

    Nuevo desarrollo

--- pre/js/main.js
+++ pre/js/main.js
@@ -2,6 +2,7 @@ import html2canvas from 'html2canvas';
 import { getInTooltip, getOutTooltip, positionTooltip } from './tooltip';
 import { setRRSSLinks } from './rrss';
 import { numberWithCommas, numberWithCommas2 } from './helpers';
+import 'url-search-params-polyfill';
 import * as d3 from 'd3';
 
 //Necesario para importar los estilos de forma automática en la etiqueta 'style' del html final
@@ -476,19 +477,42 @@ function initSecondPath(data) {
 setRRSSLinks();
 
 ///// ALTURA DEL BLOQUE DEL GRÁFICO //////
-function setChartHeight() {
-    let titleBlock = document.getElementsByClassName('b-title')[0].clientHeight;
-    let logicBlock = document.getElementsByClassName('chart__logics')[0].clientHeight;
-    let footerBlock = document.getElementsByClassName('chart__footer')[0].clientHeight;
-    let footerTop = 8, containerPadding = 8, marginTitle = 8, marginLogics = 16;
+function getIframeParams() {
+    const params = new URLSearchParams(window.location.search);
+    const iframe = params.get('iframe');
 
-    //Comprobar previamente la altura que le demos al MAIN. El estado base es 588 pero podemos hacerlo más o menos alto en función de nuestros intereses
+    if(iframe == 'fijo') {
+        setChartHeight(fijo);
+    } else {
+        setChartHeight();
+    }
 
-    let height = 612; //Altura total del main | Cambiar cuando sea necesario > Quitar aquí los ejes: 35 + 27 > 62
-    document.getElementsByClassName('chart__viz')[0].style.height = height - titleBlock - logicBlock - footerBlock - footerTop - containerPadding - marginTitle - marginLogics + 'px';
+    console.log("Entra", iframe);
 }
 
-setChartHeight();
+///Si viene desde iframe con altura fija, ejecutamos esta función. Si no, los altos son dinámicos a través de PYMJS
+function setChartHeight(iframe_fijo) {
+    if(iframe_fijo) {
+        //El contenedor y el main reciben una altura fija. En este caso, 688 y 656
+        //La altura del gráfico se ajusta más a lo disponible en el main, quitando títulos, lógica, ejes y pie de gráfico
+        document.getElementsByClassName('container')[0].style.height = '688px';
+        document.getElementsByClassName('main')[0].style.height = '656px';
+
+        let titleBlock = document.getElementsByClassName('b-title')[0].clientHeight;
+        let logicBlock = document.getElementsByClassName('chart__logics')[0].clientHeight;
+        let footerBlock = document.getElementsByClassName('chart__footer')[0].clientHeight;
+        let footerTop = 8, containerPadding = 8, marginTitle = 8, marginLogics = 16;
+
+        //Comprobar previamente la altura que le demos al MAIN. El estado base es 588 pero podemos hacerlo más o menos alto en función de nuestros intereses
+
+        let height = 612; //Altura total del main | Cambiar cuando sea necesario > Quitar aquí los ejes: 35 + 27 > 62
+        document.getElementsByClassName('chart__viz')[0].style.height = height - titleBlock - logicBlock - footerBlock - footerTop - containerPadding - marginTitle - marginLogics + 'px';
+    } else {
+        document.getElementsByClassName('main')[0].style.height = document.getElementsByClassName('main')[0].clientHeight + 'px';
+    }    
+}
+
+getIframeParams();
 
 ///// DESCARGA COMO PNG O SVG > DOS PASOS/////
 let innerCanvas;
